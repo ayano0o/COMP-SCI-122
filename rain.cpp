5255afcb165f9f25f94369effbc67dbc62a3f01a
ayano0o <123913796+ayano0o@users.noreply.github.com>
2024-02-01 16:48:46 -0600

    Create rain.cpp

---
+++ rain.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <limits>
+
+#include "point.h"
+#include "city.h"
+#include "cityINFO.h"
+using namespace std;
+
+int main() {
+    CityMap cityMap;
+    cityMap.menu();
+
+    return 0;
+}
